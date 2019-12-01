6a3da13d95f967d27aea7c9ed274f19de0f0e1e3
Jason Zahn <smedge@gmail.com>
2019-12-01 13:31:07 -0600

    beginning of o2 toxicity meter. also some cleanup.

---
+++ src/blacktip/o2toxicity.cpp
@@ -0,0 +1,10 @@
+#include "o2toxicity.h"
+
+namespace blacktip
+{
+    void O2ToxicityMeter::calculate(const unsigned long millis, const double depth,
+            const Mix &mix)
+    {
+
+    }
+}
