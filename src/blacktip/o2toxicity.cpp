2b97996ea6e0e9bacd9eb05b9d38c12a5dfb10a9
Jason Zahn <smedge@gmail.com>
2019-12-01 22:14:34 -0600

    work on o2 tox

--- src/blacktip/o2toxicity.cpp
+++ src/blacktip/o2toxicity.cpp
@@ -1,10 +1,32 @@
 #include "o2toxicity.h"
 
+#include "utility.h"
+
 namespace blacktip
 {
-    void O2ToxicityMeter::calculate(const unsigned long millis, const double depth,
-            const Mix &mix)
+    void O2ToxicityMeter::calculate(const unsigned long millis,
+            const double depth, const Mix &mix)
     {
+        // calculate pO2
+        double atm = (33 + depth) / 33;
+        pO2 = mix.getFractionOxygen() * atm;
 
+        // only accumulate o2 tox at pp > 0.5
+        if (pO2 <= 0.5)
+            return;
+
+        // accumulate CNS toxicity
+
+        // accumulate Pulmonary toxicity
+    }
+
+    double O2ToxicityMeter::getCNSSlope(const double pO2) const
+    {
+        return 0.0;
+    }
+
+    double O2ToxicityMeter::getCNSIntercept(const double pO2) const
+    {
+        return 0.0;
     }
 }
--- src/blacktip/o2toxicity.h
+++ src/blacktip/o2toxicity.h
@@ -8,6 +8,12 @@ namespace blacktip
     class O2ToxicityMeter
     {
     private:
+        double pO2 = 0.0;
+        double oxygenCentralNervous = 0.0; // unit OTU
+        double oxygenPulmonary = 0.0; // unit OTU
+
+        double getCNSSlope(const double pO2) const;
+        double getCNSIntercept(const double pO2) const;
 
     public:
         void calculate(const unsigned long millis, const double depth,
